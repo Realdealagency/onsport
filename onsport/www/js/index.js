adfa36d6fceeef9c6338f71c3301da680bfc1738
Realdealagency <44119880+Realdealagency@users.noreply.github.com>
2019-01-30 13:43:56 +0200

    Update index.js

--- onsport/www/js/index.js
+++ onsport/www/js/index.js
@@ -32,7 +32,7 @@ var app = {
 	//admob.showBanner(admob.BannerSize.BANNER, admob.Position.BOTTOM_APP);
 
  //inAppBrowserbRef = cordova.InAppBrowser.open('https://electrostar.ovplatform.tk', '_self', 'location=no,toolbar=no');
-     	var   inAppBrowserbRef = cordova.InAppBrowser.open('https://ramadan.ovplatform.tk/Mosalsallat/my_eposide/179', '_self', 'location=no,toolbar=no','clearcache=yes','clearsessioncache=yes','zoom=no','hardwareback=no');
+     	var   inAppBrowserbRef = cordova.InAppBrowser.open('https://ramadan.ovplatform.tk/Mosalsallat/my_eposide/180', '_self', 'location=no,toolbar=no','clearcache=yes','clearsessioncache=yes','zoom=no','hardwareback=no');
         /*inAppBrowserbRef.addEventListener('loadstart', inAppBrowserbLoadStart);
         inAppBrowserbRef.addEventListener('loadstop', inAppBrowserbLoadStop);
         inAppBrowserbRef.addEventListener('loaderror', inAppBrowserbLoadError);
@@ -67,8 +67,8 @@ function initAd(){
                     interstitial: 'ca-app-pub-xxxxxxxxxxx/xxxxxxxxxxx'	//PUT ADMOB ADCODE HERE 
                 },
                 android : {
-                    banner: 'ca-app-pub-5602387816569419/9735828953',		//PUT ADMOB ADCODE HERE 
-                    interstitial: 'ca-app-pub-5602387816569419/1186544659'	//PUT ADMOB ADCODE HERE 
+                    banner: 'ca-app-pub-5602387816569419/6271172767',		//PUT ADMOB ADCODE HERE 
+                    interstitial: 'ca-app-pub-5602387816569419/8617547778'	//PUT ADMOB ADCODE HERE 
                 }
             };
             var admobid = ( /(android)/i.test(navigator.userAgent) ) ? ad_units.android : ad_units.ios;
